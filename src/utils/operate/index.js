97eb000620fd1e827c0477f665d797dea60e07bd
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-09-01 15:01:17 -0400

    Create utilts/operate

---
+++ src/utils/operate/index.js
@@ -0,0 +1,16 @@
+import operations from './operations';
+
+/**
+ * Handle an operation between two numbers
+ * @param {*} operator Operator name
+ * @param {*} b Second number
+ * @param {*} a first number
+ */
+const operate = (operator, b, a) => {
+  const firstNum = Number(a);
+  const secondNum = Number(b);
+
+  return operations[operator](firstNum, secondNum);
+}
+
+export default operate;
