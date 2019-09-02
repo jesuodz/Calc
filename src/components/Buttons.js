569e866eee38159d0af1c5b3ad3088cca7c2147e
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-09-02 06:12:55 -0400

    Refactor buttons pad

---
+++ src/components/Buttons.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Buttons = (
+  { handleOperator, handleOperand, handleClear }
+) => (
+  <div className='buttons'>
+    <div className='button-container'>
+      <button id='button7' value='7' onClick={handleOperand}>7</button>
+      <button id='button8' value='8' onClick={handleOperand}>8</button>
+      <button id='button9' value='9' onClick={handleOperand}>9</button>
+      <button id='multiply' onClick={handleOperator}>x</button>
+    </div>
+    <div className='button-container'>
+      <button id='button4' value='4' onClick={handleOperand}>4</button>
+      <button id='button5' value='5' onClick={handleOperand}>5</button>
+      <button id='button6' value='6' onClick={handleOperand}>6</button>
+      <button id='divide' onClick={handleOperator}>&#247;</button>
+    </div>
+    <div className='button-container'>
+      <button id='button1' value='1' onClick={handleOperand}>1</button>
+      <button id='button2' value='2' onClick={handleOperand}>2</button>
+      <button id='button3' value='3' onClick={handleOperand}>3</button>
+      <button id='add' onClick={handleOperator}>+</button>
+    </div>
+    <div className='button-container'>
+      <button id='button0' value='0' onClick={handleOperand}>0</button>
+      <button id='clearButton' onClick={handleClear}>C</button>
+      <button id='equalsButton'>=</button>
+      <button id='substract' onClick={handleOperator}>-</button>
+      <button id='decimal' onClick={handleOperand}>.</button>
+    </div>
+  </div>
+);
+
+Buttons.propTypes = {
+  handleOperand: PropTypes.func.isRequired,
+  handleClear: PropTypes.func.isRequired,
+  handleOperator: PropTypes.func.isRequired
+};
+
+export default Buttons;
--- src/components/KeyPad.js
+++ src/components/KeyPad.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Buttons from './Buttons';
 
 import { clear, addOperand, addOperator } from '../actions';
 
@@ -25,33 +26,11 @@ class KeyPad extends Component {
 
   render() {
     return (
-      <div className='buttons'>
-        <div className='button-container'>
-          <button id='button7' value='7' onClick={this.handleOperand}>7</button>
-          <button id='button8' value='8' onClick={this.handleOperand}>8</button>
-          <button id='button9' value='9' onClick={this.handleOperand}>9</button>
-          <button id='multiply' onClick={this.handleOperator}>x</button>
-        </div>
-        <div className='button-container'>
-          <button id='button4' value='4' onClick={this.handleOperand}>4</button>
-          <button id='button5' value='5' onClick={this.handleOperand}>5</button>
-          <button id='button6' value='6' onClick={this.handleOperand}>6</button>
-          <button id='divide' onClick={this.handleOperator}>&#247;</button>
-        </div>
-        <div className='button-container'>
-          <button id='button1' value='1' onClick={this.handleOperand}>1</button>
-          <button id='button2' value='2' onClick={this.handleOperand}>2</button>
-          <button id='button3' value='3' onClick={this.handleOperand}>3</button>
-          <button id='add' onClick={this.handleOperator}>+</button>
-        </div>
-        <div className='button-container'>
-          <button id='button0' value='0' onClick={this.handleOperand}>0</button>
-          <button id='clearButton' onClick={this.props.clear}>C</button>
-          <button id='equalsButton'>=</button>
-          <button id='substract' onClick={this.handleOperator}>-</button>
-          <button id='decimal' onClick={this.handleOperand}>.</button>
-        </div>
-      </div>
+      <Buttons
+        handleOperand={this.handleOperand}
+        handleOperator={this.handleOperator}
+        handleClear={this.props.clear}
+      />
     );
   }
 };
